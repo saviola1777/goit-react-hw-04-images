97ae93156b86d04fcae947d75ad3fa58ceb93341
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-02-07 21:30:12 +0200

    change modal onclick on escape

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,52 +1,57 @@
-import css from 'components/Modal/Modal.module.css'
-import {useEffect , useCallback} from 'react'
-import PropTypes from 'prop-types';
-import { createPortal } from 'react-dom'  // Імпорт з реакт дому для встановлення модалки поверх всіх елементів
+  import css from 'components/Modal/Modal.module.css'
+  import {useEffect } from 'react'
+  import PropTypes from 'prop-types';
+  import { createPortal } from 'react-dom'  // Імпорт з реакт дому для встановлення модалки поверх всіх елементів
 
-const modalRoot = document.querySelector('#modal-root')    //доступ до нашого діва в якому буде модалка який знаходиться по верх всіх елементів
+  const modalRoot = document.querySelector('#modal-root')    //доступ до нашого діва в якому буде модалка який знаходиться по верх всіх елементів
 
-const Modal =({largeImg, close })=>{                          // передаємо пропсами велику картинку і функцію яка закриває модалку міняє на false
- 
-  const closeModal =useCallback(({ target, currentTarget, code }) => {   // деструктупизація на що саме ми клікнули code це назва клавіши
-    if (target === currentTarget || code === "Escape") {      //якшо клікнемо поза картинкою або нажмемо на ускейп виклече функцію яка закриває модалку
-      close()
-    }
-  },[close])
+  const Modal =({largeImg, close })=>{                          // передаємо пропсами велику картинку і функцію яка закриває модалку міняє на false
   
-  useEffect(()=>{     
- document.addEventListener("keydown", closeModal) ; 
-return ()=>document.removeEventListener("keydown", closeModal);
-  },[closeModal])
+    const closeModalClick =({ target, currentTarget }) => {   // деструктупизація на що саме ми клікнули code це назва клавіши
+      if (target === currentTarget ) {      //якшо клікнемо поза картинкою або нажмемо на ускейп виклече функцію яка закриває модалку
+        close()
+      }
+    }
+    
+    useEffect(()=>{   
+      const closeModal =({code }) => {   // деструктупизація на що саме ми клікнули code це назва клавіши
+        if (code === "Escape") {      //якшо клікнемо поза картинкою або нажмемо на ускейп виклече функцію яка закриває модалку
+          close()
+        }
+      }  
+  document.addEventListener("keydown", closeModal) ; 
+  return ()=>document.removeEventListener("keydown", closeModal);
+    },[close])
 
-return (
+  return (
 
-    createPortal(                // createPortal ---мнтод де першим аргументом ми кажем що відмалювати , а другий доступ до діва куда саме ми одмалюємо модалку
+      createPortal(                // createPortal ---мнтод де першим аргументом ми кажем що відмалювати , а другий доступ до діва куда саме ми одмалюємо модалку
 
-      <div className={css.overlay} onClick={closeModal}>
-        <div className={css.modal}>
-          <span className={css.span} onClick={close}>X</span>
-          <img className={css.img} src={largeImg.largeImageURL} alt={largeImg.tags} />
-        </div>
-      </div>,
-      modalRoot    // другий елемент це доступ до нашого діва який знаходиться по верх всіх елементів
-    ))
-}
-
-
-Modal.propTypes = {
-  close: PropTypes.func.isRequired,
-  largeImg: PropTypes.object.isRequired,
-};
-export default Modal
+        <div className={css.overlay} onClick={closeModalClick}>
+          <div className={css.modal}>
+            <span className={css.span} onClick={close}>X</span>
+            <img className={css.img} src={largeImg.largeImageURL} alt={largeImg.tags} />
+          </div>
+        </div>,
+        modalRoot    // другий елемент це доступ до нашого діва який знаходиться по верх всіх елементів
+      ))
+  }
 
 
+  Modal.propTypes = {
+    close: PropTypes.func.isRequired,
+    largeImg: PropTypes.object.isRequired,
+  };
+  export default Modal
 
-// const closeModal =useCallback(({ target, currentTarget, code }) => {   // деструктупизація на що саме ми клікнули code це назва клавіши
-//   if (target === currentTarget || code === "Escape") {      //якшо клікнемо поза картинкою або нажмемо на ускейп виклече функцію яка закриває модалку
-//     close()
-//   }
-// },[close])
-// useEffect(()=>{     
-// document.addEventListener("keydown", closeModal) ; 
-// return ()=>document.removeEventListener("keydown", closeModal);
-// },[closeModal])
+
+
+  // const closeModal =useCallback(({ target, currentTarget, code }) => {   // деструктупизація на що саме ми клікнули code це назва клавіши
+  //   if (target === currentTarget || code === "Escape") {      //якшо клікнемо поза картинкою або нажмемо на ускейп виклече функцію яка закриває модалку
+  //     close()
+  //   }
+  // },[close])
+  // useEffect(()=>{     
+  // document.addEventListener("keydown", closeModal) ; 
+  // return ()=>document.removeEventListener("keydown", closeModal);
+  // },[closeModal])
